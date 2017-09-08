166f4ff745276c5b304d2e3862b61bd37e3f909e
htkuan <kst920106@gmail.com>
2017-09-08 12:00:51 +0800

    add data structures

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Data-Structures-and-Algorithms
 Using python to implement algorithms and data structures
+
+main learning source:
+><http://interactivepython.org/runestone/static/pythonds/index.html>
---
+++ data_structures/Queue.py
@@ -0,0 +1,19 @@
+class Queue():
+    '''
+    using list
+    first-in first-out
+    '''
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item): #往 list 前面加item
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
